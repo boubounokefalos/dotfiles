d855bf88dd2f9541eb7b917eeb65d695d94b557f
DaRk_dOg <844923+gkatsiapis@users.noreply.github.com>
2018-04-07 11:48:07 +0300

    simple ubuntu updating script

--- .config/openbox/update.sh
+++ .config/openbox/update.sh
@@ -1,11 +1,29 @@
 #! /bin/bash
 
+##################################################
+#   Simple script to update your Ubuntu system   #
+#                 by DaRk_dOg                    #
+##################################################
+
 read -p "This will update your system. Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "Updater needs root privilege"
   sleep 2
-  exec sudo apt update &
-  exec sudo apt upgrade
+  sudo apt update &&
+  sudo apt upgrade &&
+  sleep 1
+  echo "."
+  sleep 1
+  echo "."
+
+  sleep 1
+  echo "."
+
+  sleep 1
+  echo "."
+
+  echo "System Updated!"
+  sleep 2
 else
   echo "Aborting System Update..."
   sleep 3
